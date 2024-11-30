e911478e8b5a1830b653a8e350e7704d24b2d00f
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2024-11-30 08:26:35 +0530

    Update app.py

--- service-a/app.py
+++ service-a/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 import requests
 import pika
-import time
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.pika import PikaInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -16,7 +16,11 @@ RequestsInstrumentor().instrument()
 trace_provider = TracerProvider()
 jaeger_exporter = JaegerExporter(agent_host_name="jaeger-agent", agent_port=6831)
 trace_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
+
 app.config["OPENTELEMETRY_PROVIDER"] = trace_provider
+
+# Instrument RabbitMQ
+PikaInstrumentor().instrument(tracer_provider=trace_provider)
 
 @app.route("/service-a")
 def service_a():
